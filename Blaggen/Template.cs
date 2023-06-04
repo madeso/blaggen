a64672343acd0002b15952f0ff0d36c9618716f9
Gustav <sir.gustav.the.coder@gmail.com>
2023-06-04 12:43:58 +0200

    Add parser with improved error handling and syntax

--- Blaggen/Template.cs
+++ Blaggen/Template.cs
@@ -1,6 +1,16 @@
-﻿using System.Globalization;
+﻿using Spectre.Console;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Data.SqlTypes;
+using System.Diagnostics.CodeAnalysis;
+using System.Drawing;
+using System.Globalization;
+using System.Runtime.CompilerServices;
+using System.Security.Claims;
 using System.Text;
 using System.Xml.Linq;
+using static Blaggen.Template;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Blaggen;
@@ -19,7 +29,7 @@ var str = generator(foo);
 */
 
 
-public class Template
+public static class Template
 {
     public delegate string Func(List<string> args);
 
@@ -92,6 +102,453 @@ public class Template
         public const char BEGIN_SIGN = '(';
         public const char END_SIGN = ')';
         public const char SEP_SIGN = ',';
+    }
+
+    public enum TokenType
+    {
+        Text,
+        Begin, End,
+        BeginTrim, EndTrim,
+        Ident,
+        Dot,
+        Comma,
+        Pipe,
+        LeftParen, RightParen,
+        Hash,
+        Slash,
+        Eof,
+    }
+
+    public record Location(int Line, int Offset);
+    public record Token(TokenType Type, string Lexeme, Location Location, string Value);
+    private record ScannerLocation(int Line, int Offset, int Index);
+
+    public record Error(Location Location, string Message);
+
+    public static (ImmutableArray<Token>, ImmutableArray<Error>) Scanner(string source)
+    {
+        var start = new ScannerLocation(1, 0, 0);
+        var current = start;
+        var inside = false;
+
+        var errors = new List<Error>();
+        var ret = new List<Token>();
+
+        while (false == IsAtEnd())
+        {
+            start = current;
+            ret.AddRange(ScanToken());
+        }
+        ret.Add(new Token(TokenType.Eof, "", new Location(current.Line, current.Offset), string.Empty));
+
+        if (errors.Count != 0)
+        {
+            ret.Clear();
+        }
+        return (ret.ToImmutableArray(), errors.ToImmutableArray());
+
+        void ReportError(Location loc, string message)
+        {
+            errors.Add(new Error(loc, message));
+        }
+
+        IEnumerable<Token> ScanToken()
+        {
+            if (inside)
+            {
+                var tok = ScanInsideToken();
+                if (tok != null) yield return tok;
+            }
+            else
+            {
+                while (inside == false && false == IsAtEnd())
+                {
+                    var beforeStart = current;
+                    var c = Advance();
+                    if (c == '{' && Match('{'))
+                    {
+                        var beginType = TokenType.Begin;
+                        if (Peek() == '-')
+                        {
+                            Advance();
+                            beginType = TokenType.BeginTrim;
+                        }
+                        var afterStart = current;
+                        var text = AddToken(TokenType.Text, null, start, beforeStart);
+                        if(text.Value.Length > 0)
+                        {
+                            yield return text;
+                        }
+                        inside = true;
+
+                        yield return AddToken(beginType, null, beforeStart, current);
+                    }
+                }
+
+                if (IsAtEnd())
+                {
+                    var text = AddToken(TokenType.Text);
+                    if (text.Value.Length > 0)
+                    {
+                        yield return text;
+                    }
+                }
+            }
+        }
+
+        Token? ScanInsideToken()
+        {
+            var c = Advance();
+            switch (c)
+            {
+                case '-':
+                    if (!Match('}'))
+                    {
+                        ReportError(StartLocation(), "Detected rouge -");
+                        return null;
+                    }
+
+                    if (!Match('}'))
+                    {
+                        ReportError(StartLocation(), "Detected rouge -}");
+                        return null;
+                    }
+
+                    inside = false;
+                    return AddToken(TokenType.EndTrim);
+                case '}':
+                    if (!Match('}'))
+                    {
+                        ReportError(StartLocation(), "Detected rouge {");
+                        return null;
+                    }
+
+                    inside = false;
+                    return AddToken(TokenType.End);
+                case '|': return AddToken(TokenType.Pipe);
+                case ',': return AddToken(TokenType.Comma);
+                case '(': return AddToken(TokenType.LeftParen);
+                case ')': return AddToken(TokenType.RightParen);
+                case '#': return AddToken(TokenType.Hash);
+                case '.': return AddToken(TokenType.Dot);
+
+                case '/':
+                    if(!Match('*')) {return AddToken(TokenType.Slash);}
+                    while (Peek() != '*' && PeekNext() != '/'  && !IsAtEnd())
+                    {
+                        Advance();
+                    }
+
+                    // skip * and /
+                    if (!IsAtEnd())
+                    {
+                        Advance();
+                        Advance();
+                    }
+
+                    return null;
+
+                case '"':
+                    while (Peek() != '"' && !IsAtEnd())
+                    {
+                        Advance();
+                    }
+
+                    if (IsAtEnd())
+                    {
+                        ReportError(StartLocation(), "Unterminated string.");
+                        return null;
+                    }
+
+                    // The closing ".
+                    Advance();
+
+                    // Trim the surrounding quotes.
+                    var value = source.Substring(start.Index + 1, current.Index - start.Index - 2);
+                    return AddToken(TokenType.Ident, value);
+
+                case ' ':
+                case '\r':
+                case '\n':
+                case '\t':
+                    return null;
+
+                default:
+                    if (IsDigit(c))
+                    {
+                        while (IsDigit(Peek())) Advance();
+
+                        // Look for a fractional part.
+                        if (Peek() == '.' && IsDigit(PeekNext()))
+                        {
+                            // Consume the "."
+                            Advance();
+
+                            while (IsDigit(Peek())) Advance();
+                        }
+
+                        return AddToken(TokenType.Ident);
+                    }
+                    else if (IsAlpha(c))
+                    {
+                        while (IsAlphaNumeric(Peek())) Advance();
+
+                        return AddToken(TokenType.Ident);
+                    }
+                    else
+                    {
+                        ReportError(StartLocation(), $"Unexpected character {c}");
+                        return null;
+                    }
+            }
+        }
+
+        static bool IsDigit(char c)
+        {
+            return c is >= '0' and <= '9';
+        }
+
+        static bool IsAlpha(char c)
+        {
+            return c is >= 'a' and <= 'z' or >= 'A' and <= 'Z' or '_';
+        }
+
+        static bool IsAlphaNumeric(char c)
+        {
+            return IsAlpha(c) || IsDigit(c);
+        }
+
+        ScannerLocation NextChar()
+        {
+            return current with { Index = current.Index + 1, Offset = current.Offset + 1 };
+        }
+
+        bool Match(char expected)
+        {
+            if (IsAtEnd()) { return false; }
+            if (source[current.Index] != expected) { return false; }
+
+            current = NextChar();
+            return true;
+        }
+
+        char Peek()
+        {
+            return IsAtEnd() ? '\0' : source[current.Index];
+        }
+
+        char PeekNext()
+        {
+            if (current.Index + 1 >= source.Length) return '\0';
+            return source[current.Index + 1];
+        }
+
+        char Advance()
+        {
+            var ret = source[current.Index];
+            current = NextChar();
+            if (ret == '\n')
+            {
+                current = current with { Offset = 0, Line = current.Line + 1 };
+            }
+            return ret;
+        }
+
+        Location StartLocation(ScannerLocation? stt = null)
+        {
+            var st = stt ?? start;
+            return new Location(st.Line, st.Offset);
+        }
+
+        Token AddToken(TokenType tt, string? value = null, ScannerLocation? begin = null, ScannerLocation? end = null)
+        {
+            var st = begin ?? start;
+            var cu = end ?? current;
+            var text = source.Substring(st.Index, cu.Index-st.Index);
+            return new Token(tt, text, StartLocation(st), value ?? text);
+        }
+
+        bool IsAtEnd()
+        {
+            return current.Index >= source.Length;
+        }
+    }
+
+    private class ParseError : Exception { }
+
+    public static (Node, ImmutableArray<Error>) Parse(string source, Dictionary<string, Func> functions)
+    {
+        var (tokens, lexerErrors) = Scanner(source);
+        if (lexerErrors.Length > 0)
+        {
+            return (new Node.Text("Lexing failed"), lexerErrors);
+        }
+
+        return Parse(tokens, functions);
+    }
+
+    private static (Node, ImmutableArray<Error>) Parse(ImmutableArray<Token> tokens, Dictionary<string, Func> functions)
+    {
+        // todo(Gustav): merge similar "empty" tokens, and replace trims with regular begin/end
+        var current = 0;
+
+        var nodes = new List<Node>();
+        var errors = new List<Error>();
+
+        while (!IsAtEnd())
+        {
+            try
+            {
+                ParseNode();
+            }
+            catch (ParseError)
+            {
+                Synchronize();
+            }
+        }
+
+        if(errors.Count == 0) {
+            return (new Node.Group(nodes), errors.ToImmutableArray());
+        }
+        return (new Node.Text("Parsing failed"),  errors.ToImmutableArray());
+
+        ParseError ReportError(Location loc, string message)
+        {
+            errors.Add(new Error(loc, message));
+            return new ParseError();
+        }
+
+        bool Match(params TokenType[] types)
+        {
+            if (!types.Any(Check)) return false;
+
+            Advance();
+            return true;
+        }
+
+        bool Check(TokenType type)
+        {
+            if (IsAtEnd()) return false;
+            return Peek().Type == type;
+        }
+
+        Token Advance()
+        {
+            if (!IsAtEnd()) current++;
+            return Previous();
+        }
+
+        bool IsAtEnd()
+        {
+            return Peek().Type == TokenType.Eof;
+        }
+
+        Token Peek()
+        {
+            return tokens[current];
+        }
+
+        Token Previous()
+        {
+            return tokens[current - 1];
+        }
+
+        void Synchronize()
+        {
+            Advance();
+
+            while (!IsAtEnd())
+            {
+                if (Previous().Type == TokenType.End) return;
+
+                switch (Peek().Type)
+                {
+                    case TokenType.Text:
+                        return;
+                }
+
+                Advance();
+            }
+        }
+
+        static string TokenToMessage(Token token)
+        {
+            var value = token.Type == TokenType.Text ? "" : $": {token.Lexeme}";
+            return $"{token.Type}{value}";
+        }
+
+        Token Consume(TokenType type, string message)
+        {
+            if (Check(type)) return Advance();
+
+            throw ReportError(Peek().Location, message);
+        }
+
+        Node ParseFunctionArg()
+        {
+            if(Peek().Type != TokenType.Ident)
+            {
+                throw ReportError(Peek().Location, $"Expected identifier but found {TokenToMessage(Peek())}");
+            }
+
+            return new Node.Text(Advance().Value);
+        }
+
+        void ParseNode()
+        {
+            switch (Peek().Type)
+            {
+                case TokenType.Begin:
+                    Advance();
+                    if (Peek().Type != TokenType.Ident)
+                    {
+                        throw ReportError(Peek().Location, $"Expected IDENT, found {TokenToMessage(Peek())}");
+                    }
+
+                    Node node = new Node.Attribute(Advance().Value);
+
+                    while (Peek().Type == TokenType.Pipe)
+                    {
+                        Advance();
+                        var name = Consume(TokenType.Ident, $"Expected function name but found {TokenToMessage(Peek())}");
+                        var arguments = new List<Node> { node };
+
+                        if (Match(TokenType.LeftParen))
+                        {
+                            while (Peek().Type != TokenType.RightParen && !IsAtEnd())
+                            {
+                                arguments.Add(ParseFunctionArg());
+
+                                if (Peek().Type != TokenType.RightParen)
+                                {
+                                    Consume(TokenType.Comma, $"Expected comma for the next function argument but found {TokenToMessage(Peek())}");
+                                }
+                            }
+
+                            Consume(TokenType.RightParen, $"Expected ) to end function but found {TokenToMessage(Peek())}");
+                        }
+
+                        if (functions.TryGetValue(name.Value, out var f))
+                        {
+                            node = new Node.FunctionCall(name.Value, f, arguments);
+                        }
+                        else
+                        {
+                            ReportError(name.Location, $"Unknown function named {name.Value}");
+                        }
+                    }
+                    nodes.Add(node);
+
+                    Consume(TokenType.End, $"Expected end token but found {TokenToMessage(Peek())}");
+                    break;
+                case TokenType.Text:
+                    var text = Advance();
+                    nodes.Add(new Node.Text(text.Value));
+                    break;
+                default:
+                    throw ReportError(Peek().Location, $"Unexpected token {TokenToMessage(Peek())}");
+            }
+        }
     }
 
     private static class Parser
@@ -168,7 +625,6 @@ public class Template
                         break;
                     default:
                         throw new InvalidState();
-                        break;
                 }
             }
 
--- BlaggenTest/TemplateTest.cs
+++ BlaggenTest/TemplateTest.cs
@@ -7,20 +7,208 @@ namespace BlaggenTest;
 
 public class TemplateTest
 {
-    [Fact]
-    public void Test()
+    private readonly Dictionary<string, string> abba = new()
     {
-        var data = new Dictionary<string, string>
-        {
-            {"artist", "ABBA"},
-            {"title", "dancing queen"},
-            {"album", "Arrival"},
-            { "track", "2"}
-        };
-        var f = Template.DefaultFunctions();
+        {"artist", "ABBA"},
+        {"title", "dancing queen"},
+        {"album", "Arrival"},
+        { "track", "2"}
+    };
 
-        Template.Compile("%artist% - %title% (%album%)", f).Evaluate(data).Should().Be("ABBA - dancing queen (Arrival)");
-        Template.Compile("%artist% - $title(%title%) (%album%)", f).Evaluate(data).Should().Be("ABBA - Dancing Queen (Arrival)");
-        Template.Compile("$zfill(%track%,3). $title(%title%)", f).Evaluate(data).Should().Be("002. Dancing Queen");
+    [Fact]
+    public void Test1()
+    {
+        using (new AssertionScope())
+        {
+            var (node, errors) = Template.Parse("{{artist}} - {{title}} ({{album}})", Template.DefaultFunctions());
+            node.Evaluate(abba).Should().Be("ABBA - dancing queen (Arrival)");
+            errors.Should().BeEquivalentTo(new Template.Error[] { });
+        }
+    }
+
+    [Fact]
+    public void Test2()
+    {
+        using (new AssertionScope())
+        {
+            var (node, errors) = Template.Parse("{{artist}} - {{title | title}} ({{album}})", Template.DefaultFunctions());
+            node.Evaluate(abba).Should().Be("ABBA - Dancing Queen (Arrival)");
+            errors.Should().BeEquivalentTo(new Template.Error[] { });
+        }
+    }
+
+    [Fact]
+    public void Test3()
+    {
+        using (new AssertionScope())
+        {
+            var (node, errors) = Template.Parse("{{track | zfill(3)}}. {{title | title}}", Template.DefaultFunctions());
+            node.Evaluate(abba).Should().Be("002. Dancing Queen");
+
+            errors.Should().BeEquivalentTo(new Template.Error[] { });
+        }
+    }
+
+    [Fact]
+    public void ScannerTest1()
+    {
+        using (new AssertionScope())
+        {
+            var (scanned, errors) = Template.Scanner("");
+            scanned.Should().BeEquivalentTo(new[]
+                { new Template.Token(Template.TokenType.Eof, "", new Template.Location(1, 0), "") });
+
+            errors.Should().BeEquivalentTo(new Template.Error[]{});
+        }
+    }
+
+    [Fact]
+    public void ScannerTest2()
+    {
+        using (new AssertionScope())
+        {
+            var (scanned, errors) = Template.Scanner("dog");
+            scanned.Should().BeEquivalentTo(new[]
+            {
+                new Template.Token(Template.TokenType.Text, "dog", new Template.Location(1, 0), "dog"),
+                new Template.Token(Template.TokenType.Eof, "", new Template.Location(1, 3), "")
+            });
+            errors.Should().BeEquivalentTo(new Template.Error[] { });
+        }
+    }
+
+    [Fact]
+    public void ScannerTest2A()
+    {
+        using (new AssertionScope())
+        {
+            var (scanned, errors) = Template.Scanner("{{}}");
+            scanned.Should().BeEquivalentTo(new[]
+            {
+                new Template.Token(Template.TokenType.Begin, "{{", new Template.Location(1, 0), "{{"),
+                new Template.Token(Template.TokenType.End, "}}", new Template.Location(1, 2), "}}"),
+                new Template.Token(Template.TokenType.Eof, "", new Template.Location(1, 4), "")
+            });
+            errors.Should().BeEquivalentTo(new Template.Error[] { });
+        }
+    }
+    
+    [Fact]
+    public void ScannerTest2B()
+    {
+        using (new AssertionScope())
+        {
+            var (scanned, errors) = Template.Scanner("{{-}}");
+            scanned.Should().BeEquivalentTo(new[]
+            {
+                new Template.Token(Template.TokenType.BeginTrim, "{{-", new Template.Location(1, 0), "{{-"),
+                new Template.Token(Template.TokenType.End, "}}", new Template.Location(1, 3), "}}"),
+                new Template.Token(Template.TokenType.Eof, "", new Template.Location(1, 5), "")
+            });
+            errors.Should().BeEquivalentTo(new Template.Error[] { });
+        }
+    }
+
+    [Fact]
+    public void ScannerTest2C()
+    {
+        using (new AssertionScope())
+        {
+            var (scanned, errors) = Template.Scanner("{{ -}}");
+            scanned.Should().BeEquivalentTo(new[]
+            {
+                new Template.Token(Template.TokenType.Begin, "{{", new Template.Location(1, 0), "{{"),
+                new Template.Token(Template.TokenType.EndTrim, "-}}", new Template.Location(1, 3), "-}}"),
+                new Template.Token(Template.TokenType.Eof, "", new Template.Location(1, 6), "")
+            });
+            errors.Should().BeEquivalentTo(new Template.Error[] { });
+        }
+    }
+
+    [Fact]
+    public void ScannerTest3()
+    {
+        using (new AssertionScope())
+        {
+            var (scanned, errors) = Template.Scanner("{{cat}}");
+            scanned.Should().BeEquivalentTo(new[]
+            {
+                new Template.Token(Template.TokenType.Begin, "{{", new Template.Location(1, 0), "{{"),
+                new Template.Token(Template.TokenType.Ident, "cat", new Template.Location(1, 2), "cat"),
+                new Template.Token(Template.TokenType.End, "}}", new Template.Location(1, 5), "}}"),
+                new Template.Token(Template.TokenType.Eof, "", new Template.Location(1, 7), "")
+            });
+            errors.Should().BeEquivalentTo(new Template.Error[] { });
+        }
+    }
+
+    [Fact]
+    public void ScannerTest4()
+    {
+        using (new AssertionScope())
+        {
+            var (scanned, errors) = Template.Scanner("awesome {{\"black cat\"");
+            scanned.Should().BeEquivalentTo(new[]
+            {
+                new Template.Token(Template.TokenType.Text, "awesome ", new Template.Location(1, 0), "awesome "),
+                new Template.Token(Template.TokenType.Begin, "{{", new Template.Location(1, 8), "{{"),
+                new Template.Token(Template.TokenType.Ident, "\"black cat\"", new Template.Location(1, 10),
+                    "black cat"),
+                new Template.Token(Template.TokenType.Eof, "", new Template.Location(1, 21), "")
+            });
+            errors.Should().BeEquivalentTo(new Template.Error[] { });
+        }
+    }
+
+    [Fact]
+    public void ScannerTest5()
+    {
+        using (new AssertionScope())
+        {
+            var (scanned, errors) = Template.Scanner("{{42 4.42}} { dog {");
+            scanned.Should().BeEquivalentTo(new[]
+            {
+                new Template.Token(Template.TokenType.Begin, "{{", new Template.Location(1, 0), "{{"),
+                new Template.Token(Template.TokenType.Ident, "42", new Template.Location(1, 2), "42"),
+                new Template.Token(Template.TokenType.Ident, "4.42", new Template.Location(1, 5), "4.42"),
+                new Template.Token(Template.TokenType.End, "}}", new Template.Location(1, 9), "}}"),
+                new Template.Token(Template.TokenType.Text, " { dog {", new Template.Location(1, 11), " { dog {"),
+                new Template.Token(Template.TokenType.Eof, "", new Template.Location(1, 19), "")
+            });
+            errors.Should().BeEquivalentTo(new Template.Error[] { });
+        }
+    }
+
+    [Fact]
+    public void ScannerTest6()
+    {
+        using (new AssertionScope())
+        {
+            var (scanned, errors) = Template.Scanner("{{}}");
+            scanned.Should().BeEquivalentTo(new[]
+            {
+                new Template.Token(Template.TokenType.Begin, "{{", new Template.Location(1, 0), "{{"),
+                new Template.Token(Template.TokenType.End, "}}", new Template.Location(1, 2), "}}"),
+                new Template.Token(Template.TokenType.Eof, "", new Template.Location(1, 4), "")
+            });
+            errors.Should().BeEquivalentTo(new Template.Error[] { });
+        }
+    }
+
+    [Fact]
+    public void ScannerTest7()
+    {
+        using (new AssertionScope())
+        {
+            var (scanned, errors) = Template.Scanner("{{ /* this is a test */ }}");
+            scanned.Should().BeEquivalentTo(new[]
+            {
+                new Template.Token(Template.TokenType.Begin, "{{", new Template.Location(1, 0), "{{"),
+                new Template.Token(Template.TokenType.End, "}}", new Template.Location(1, 24), "}}"),
+                new Template.Token(Template.TokenType.Eof, "", new Template.Location(1, 26), "")
+            });
+
+            errors.Should().BeEquivalentTo(new Template.Error[] { });
+        }
     }
 }
