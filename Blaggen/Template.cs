42ca70fb46dbe69f4f8673e1593113b1e963a06e
Gustav <sir.gustav.the.coder@gmail.com>
2023-06-18 10:44:08 +0200

    Cleanup

--- Blaggen/Template.cs
+++ Blaggen/Template.cs
@@ -1,30 +1,41 @@
 ﻿using System.Collections.Immutable;
 using System.Globalization;
-using System.IO;
 using System.Text;
 
 namespace Blaggen;
 
+/*
+
+  API:
+    var (generator, error) = Template.Parse(...);
+    string ret = generator(myClass);
+
+  Template syntax:
+    {{ prop }} {{- "also prop, trim printable spaces" -}}
+    {{prop | function | function(with_arguments)}}
+    {{include file}} {{include "file/with.extension"}}
+    {{#list}}repeated{{/list}} {{range also_list}}repeated{{end}}
+    {{if bool_prop}}perhaps{{end}}
+
+*/
+
 // todo(Gustav): add foo.bar and .foobar accessors to access subdicts and global dicts
-// todo(Gustav): add boolean expressions...? probably not
-// todo(Gustav): add documentation to properties and functions so error messages can be more helpful
+// todo(Gustav): add documentation to properties and functions so error messages can be more helpful, this also means we can generate documentation for the current build
 // todo(Gustav): should return values always be strings? properties return datetime that format functions could format
 
 public static class Template
 {
+    public record Location(FileInfo File, int Line, int Offset);
+    public record Error(Location Location, string Message);
+    public record FuncArgument(Location Location, string Argument);
+
     // parse arguments, return function call or error
-    public record LocationArgument(Location Location, string Argument);
-    public delegate (Func, ImmutableArray<Error>) FuncGenerator(Location call, ImmutableArray<LocationArgument> arguments);
+    public delegate (Func, ImmutableArray<Error>) FuncGenerator(Location call, ImmutableArray<FuncArgument> arguments);
 
     // apply dynamic string function and return result
     public delegate string Func(string arg);
 
-    public record Location(FileInfo File, int Line, int Offset);
-    public record Error(Location Location, string Message);
-    private static ImmutableArray<Error> NoErrors() => ImmutableArray<Error>.Empty;
-
-    private static Location UnknownLocation() => new(new FileInfo("unknown-file.txt"), -1, -1);
-
+    
     public class Definition<TParent>
     {
         private readonly Dictionary<string, Func<TParent, string>> attributes = new ();
@@ -45,25 +56,24 @@ public static class Template
 
         public Definition<TParent> AddList<TChild>(string name, Func<TParent, IEnumerable<TChild>> childSelector, Definition<TChild> childDef)
         {
-            // todo(Gustav): implement Add
             children.Add(name, node =>
             {
                 var (getter, errors) = childDef.Validate(node);
                 if (errors.Length > 0) { return (SyntaxError, errors); }
 
-                return (parent => string.Join("", childSelector(parent).Select(getter)), NoErrors());
+                return (parent => string.Join("", childSelector(parent).Select(getter)), NoErrors);
             });
             return this;
         }
 
         private static string SyntaxError(TParent _) => "Syntax error";
 
-        public (Func<TParent, string>, ImmutableArray<Error>) Validate(Node node)
+        internal (Func<TParent, string>, ImmutableArray<Error>) Validate(Node node)
         {
             switch (node)
             {
                 case Node.Text text:
-                    return (_ => text.Value, NoErrors());
+                    return (_ => text.Value, NoErrors);
                 case Node.Attribute attribute:
                 {
                     if (false == attributes.TryGetValue(attribute.Name, out var getter))
@@ -73,7 +83,7 @@ public static class Template
                                 $"Missing attribute ${attribute.Name}: {MatchStrings(attribute.Name, attributes.Keys)}"
                             )));
                     }
-                    return (parent => getter(parent), NoErrors());
+                    return (parent => getter(parent), NoErrors);
                 }
                 case Node.If check:
                 {
@@ -88,7 +98,7 @@ public static class Template
                     var (body, errors) = Validate(check.Body);
                     if (errors.Length > 0) { return (SyntaxError, errors); }
 
-                    return (parent => getter(parent) ? body(parent) : string.Empty, NoErrors());
+                    return (parent => getter(parent) ? body(parent) : string.Empty, NoErrors);
                 }
                 case Node.Iterate iterate:
                 {
@@ -106,7 +116,7 @@ public static class Template
                     var (getter, errors) = Validate(fc.Arg);
                     if (errors.Length > 0) { return (SyntaxError, errors); }
 
-                    return (parent => fc.Function(getter(parent)), NoErrors());
+                    return (parent => fc.Function(getter(parent)), NoErrors);
                 }
                 case Node.Group gr:
                 {
@@ -115,7 +125,7 @@ public static class Template
                     if (errors.Length > 0) { return (SyntaxError, errors); }
 
                     var getters = validatedArgs.Select(x => x.Item1).ToImmutableArray();
-                    return (parent => string.Join("", getters.Select(x => x(parent)).ToList()), NoErrors());
+                    return (parent => string.Join("", getters.Select(x => x(parent)).ToList()), NoErrors);
                 }
 
                 default:
@@ -123,38 +133,6 @@ public static class Template
             }
         }
     }
-
-    public abstract record Node
-    {
-        private Node() {}
-
-        internal record Text(string Value, Location Location) : Node();
-        internal record Attribute(string Name, Location Location) : Node();
-        internal record Iterate(string Name, Node Body, Location Location) : Node();
-        internal record If(string Name, Node Body, Location Location) : Node();
-        internal record FunctionCall(string Name, Func Function, Node Arg, Location Location) : Node();
-        internal record Group(List<Node> Nodes, Location Location) : Node();
-    }
-
-
-    private static string MatchStrings(string name, IEnumerable<string> candidates)
-    {
-        var all = candidates.ToImmutableArray();
-
-        var matches = EditDistance.ClosestMatches(name, 10, all).ToImmutableArray();
-
-        return matches.Length > 0
-            ? $"did you mean {ToArrayString(matches)} of {all.Length}: {ToArrayString(all)}"
-            : $"No match in {all.Length}: {ToArrayString(all)}"
-            ;
-
-        static string ToArrayString(IEnumerable<string> candidates)
-        {
-            var s = string.Join(", ", candidates);
-            return $"[{s}]";
-        }
-    }
-    
 
     public static async Task<(Func<T, string>, ImmutableArray<Error>)> Parse<T>(FileInfo path, VfsRead vfs, Dictionary<string, FuncGenerator> functions, DirectoryInfo includeDir, Definition<T> definition)
     {
@@ -191,7 +169,7 @@ public static class Template
                         ImmutableArray.Create(
                             new Error(location, "Expected zero arguments")));
                 }
-                return (arg => f(arg), NoErrors());
+                return (arg => f(arg), NoErrors);
             };
         }
         
@@ -206,8 +184,8 @@ public static class Template
             {
                 return args.Length switch
                 {
-                    0 => (arg => f(arg, missing), NoErrors()),
-                    1 => (arg => f(arg, args[0].Argument), NoErrors()),
+                    0 => (arg => f(arg, missing), NoErrors),
+                    1 => (arg => f(arg, args[0].Argument), NoErrors),
                     _ => (_ => "syntax error",
                         ImmutableArray.Create(new Error(
                             location,
@@ -222,9 +200,9 @@ public static class Template
             {
                 return args.Length switch
                 {
-                    0 => (arg => f(arg, missing), NoErrors()),
+                    0 => (arg => f(arg, missing), NoErrors),
                     1 => int.TryParse(args[0].Argument, out var number)
-                        ? (arg => f(arg, number), NoErrors())
+                        ? (arg => f(arg, number), NoErrors)
                         : (_ => "syntax error",
                             ImmutableArray.Create(
                                 new Error(location, "This function takes zero or one int argument"),
@@ -243,18 +221,18 @@ public static class Template
         t.Add("substr", IntIntArgument((arg, lhs, rhs)=> arg.Substring(lhs, rhs)));
         return t;
 
+        static (Func, ImmutableArray<Error>) SyntaxError(params Error[] errors)
+            => (_ => "syntax error", ImmutableArray.Create(errors));
+
         static FuncGenerator StringStringArgument(Func<string, string, string, string> f)
         {
             return (location, args) =>
             {
                 if (args.Length != 2)
                 {
-                    return (
-                        _ => "syntax error",
-                        ImmutableArray.Create(
-                            new Error(location, "Expected two arguments")));
+                    return SyntaxError(new Error(location, "Expected two arguments"));
                 }
-                return (arg => f(arg, args[0].Argument, args[1].Argument), NoErrors());
+                return (arg => f(arg, args[0].Argument, args[1].Argument), NoErrors);
             };
         }
 
@@ -264,37 +242,30 @@ public static class Template
             {
                 if (args.Length != 2)
                 {
-                    return (
-                        _ => "syntax error",
-                        ImmutableArray.Create(
-                            new Error(location, "Expected two arguments")));
+                    return SyntaxError(new Error(location, "Expected two arguments"));
                 }
 
                 if (int.TryParse(args[0].Argument, out var lhs) == false)
                 {
-                    return (
-                        _ => "syntax error",
-                        ImmutableArray.Create(
-                            new Error(args[0].Location, "Not a integer")));
+                    return SyntaxError(new Error(args[0].Location, "Not a integer"));
                 }
+
                 if (int.TryParse(args[1].Argument, out var rhs) == false)
                 {
-                    return (
-                        _ => "syntax error",
-                        ImmutableArray.Create(
-                            new Error(args[1].Location, "Not a integer")));
+                    return SyntaxError(new Error(args[1].Location, "Not a integer"));
                 }
-                return (arg => f(arg, lhs, rhs), NoErrors());
+
+                return (arg => f(arg, lhs, rhs), NoErrors);
             };
         }
 
         static string Capitalize(string p, bool alsoFirstChar)
         {
-            bool cap = alsoFirstChar;
-            StringBuilder sb = new StringBuilder();
-            foreach (char h in p.ToLower())
+            var cap = alsoFirstChar;
+            var sb = new StringBuilder();
+            foreach (var h in p.ToLower())
             {
-                char c = h;
+                var c = h;
                 if (char.IsLetter(c) && cap)
                 {
                     c = char.ToUpper(c);
@@ -309,6 +280,39 @@ public static class Template
 
 
     // --------------------------------------------------------------------------------------------
+
+    internal abstract record Node
+    {
+        private Node() { }
+
+        internal record Text(string Value, Location Location) : Node();
+        internal record Attribute(string Name, Location Location) : Node();
+        internal record Iterate(string Name, Node Body, Location Location) : Node();
+        internal record If(string Name, Node Body, Location Location) : Node();
+        internal record FunctionCall(string Name, Func Function, Node Arg, Location Location) : Node();
+        internal record Group(List<Node> Nodes, Location Location) : Node();
+    }
+
+    private static ImmutableArray<Error> NoErrors => ImmutableArray<Error>.Empty;
+    private static Location UnknownLocation => new(new FileInfo("unknown-file.txt"), -1, -1);
+
+    private static string MatchStrings(string name, IEnumerable<string> candidates)
+    {
+        var all = candidates.ToImmutableArray();
+
+        var matches = EditDistance.ClosestMatches(name, 10, all).ToImmutableArray();
+
+        return matches.Length > 0
+                ? $"did you mean {ToArrayString(matches)} of {all.Length}: {ToArrayString(all)}"
+                : $"No match in {all.Length}: {ToArrayString(all)}"
+            ;
+
+        static string ToArrayString(IEnumerable<string> candidates)
+        {
+            var s = string.Join(", ", candidates);
+            return $"[{s}]";
+        }
+    }
 
 
     private enum TokenType
@@ -520,25 +524,10 @@ public static class Template
             }
         }
 
-        static bool IsDigit(char c)
-        {
-            return c is >= '0' and <= '9';
-        }
-
-        static bool IsAlpha(char c)
-        {
-            return c is >= 'a' and <= 'z' or >= 'A' and <= 'Z' or '_';
-        }
-
-        static bool IsAlphaNumeric(char c)
-        {
-            return IsAlpha(c) || IsDigit(c);
-        }
-
-        ScannerLocation NextChar()
-        {
-            return current with { Index = current.Index + 1, Offset = current.Offset + 1 };
-        }
+        static bool IsDigit(char c) => c is >= '0' and <= '9';
+        static bool IsAlpha(char c) => c is >= 'a' and <= 'z' or >= 'A' and <= 'Z' or '_';
+        static bool IsAlphaNumeric(char c) => IsAlpha(c) || IsDigit(c);
+        ScannerLocation NextChar() => current with { Index = current.Index + 1, Offset = current.Offset + 1 };
 
         bool Match(char expected)
         {
@@ -556,8 +545,8 @@ public static class Template
 
         char PeekNext()
         {
-            if (current.Index + 1 >= source.Length) return '\0';
-            return source[current.Index + 1];
+            var suggestedIndex = current.Index + 1;
+            return suggestedIndex >= source.Length ? '\0' : source[suggestedIndex];
         }
 
         char Advance()
@@ -585,10 +574,7 @@ public static class Template
             return new Token(tt, text, GetStartLocation(st), value ?? text);
         }
 
-        bool IsAtEnd()
-        {
-            return current.Index >= source.Length;
-        }
+        bool IsAtEnd() => current.Index >= source.Length;
     }
 
 
@@ -717,7 +703,7 @@ public static class Template
         {
             return (rootNode, errors.ToImmutableArray());
         }
-        return (new Node.Text("Parsing failed", UnknownLocation()),  errors.ToImmutableArray());
+        return (new Node.Text("Parsing failed", UnknownLocation),  errors.ToImmutableArray());
 
         async Task<Node> ParseGroup()
         {
@@ -816,7 +802,7 @@ public static class Template
             throw ReportError(Peek().Location, message);
         }
 
-        LocationArgument ParseFunctionArg()
+        FuncArgument ParseFunctionArg()
         {
             if(Peek().Type != TokenType.Ident)
             {
@@ -824,7 +810,7 @@ public static class Template
             }
 
             var arg = Advance();
-            return new LocationArgument(arg.Location, arg.Value);
+            return new FuncArgument(arg.Location, arg.Value);
         }
 
         string ExtractAttributeName()
@@ -870,7 +856,7 @@ public static class Template
 
                         nodes.Add(new Node.If(attribute, group, start));
                     }
-                        else if (Match(TokenType.KeywordInclude))
+                    else if (Match(TokenType.KeywordInclude))
                     {
                         var name = Consume(TokenType.Ident, ExpectedMessage("IDENT"));
                         var includeLocation = Peek().Location;
@@ -941,7 +927,7 @@ public static class Template
             {
                 Advance();
                 var name = Consume(TokenType.Ident, ExpectedMessage("function name"));
-                var arguments = new List<LocationArgument>();
+                var arguments = new List<FuncArgument>();
 
                 if (Match(TokenType.LeftParen))
                 {
